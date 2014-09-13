b3672612b51595f0c2b37c4f61451de3d6ec9a5e
aman <aman.verma@hindustantimes.com>
2014-09-13 17:09:44 +0530

    basic search functionality

---
+++ search/search_indexes.py
@@ -0,0 +1,14 @@
+from haystack import indexes
+from search.models import Job
+
+
+class JobIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    type = indexes.CharField(model_attr='type', faceted=True)
+    location = indexes.CharField(model_attr='location', faceted=True)
+ 
+    def get_model(self):
+        return Job
+ 
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
